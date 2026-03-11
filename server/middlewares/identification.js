bdbaccaa30f7eafdfee19d1d6f2521ec7c61122b
matteo.borgio <matteo.borgio@gmail.com>
2026-03-11 10:39:03 +0100

    add comment in the identification.js

--- server/middlewares/identification.js
+++ server/middlewares/identification.js
@@ -1,11 +1,26 @@
+/**
+ * Middleware for web token authentication using JWT
+ * Verify token extracted from header 'Authorization' (Bearer)
+ * or from cookie. If it's valid, add the user data in the request
+ */
+
 const jwt = require('jsonwebtoken')
 
+/**
+ * JWT Identification Middleware
+ * This function checks for a valid web token in the authorization headers or cookies
+ * If verified against the TOKEN_SECRET in the env file, the user identity is added to req.user
+ * otherwise, it terminates the request with a 401 Unauthorized status
+ * @returns {Object} 401 response if unauthorized, otherwise call next()
+ */
 exports.identifier = (req, res, next) => {
     let token;
 
+    // search in the header
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
         token = req.headers.authorization;
     }
+    // otherwise search in the cookies authorization
     else if (req.cookies && req.cookies['Authorization']) {
         token = req.cookies['Authorization'];
     }
